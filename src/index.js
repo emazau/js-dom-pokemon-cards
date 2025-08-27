c4b1aef652c98cd7db31cfad29751c98d05c67f0
Emanuels Zaurins <emanuels.zaurins@no.experis.com>
2025-08-27 14:56:31 +0200

    extension complete

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ for (let i =0; i<data.length; i++){
     img.setAttribute('width','256');
     const ul = document.createElement('ul');
     ul.setAttribute('class','card--text')
+    const ul2 =  document.createElement('ul');
+    ul2.setAttribute('class','card--text')
 
     link = data[i].sprites.other["official-artwork"].front_default;
     img.setAttribute('src',link);
@@ -16,16 +18,22 @@ for (let i =0; i<data.length; i++){
     item.appendChild(h2);
     item.appendChild(img);
     item.appendChild(ul);
+    item.appendChild(createInnerHTML("p", "GAMES: "))
+
+    item.appendChild(ul2);
+
 
     for (let j = 0; j <6; j++){
         ul.appendChild(createInnerHTML("li", (data[i].stats[j].stat.name).toUpperCase() +": "+data[i].stats[j].base_stat));
     }
 
     //addgames
+
+    /** */
+    for (let j = 0; j < data[i].game_indices.length;j++) 
+        ul2.appendChild(createInnerHTML("li",  data[i].game_indices[j].version.name + ": " +data[i].game_indices[j].game_index ))
     
-    for (let j = 0; j < data[i].game_indices.length;j++){
-        let is = 0;
-    }
+        
 
     cards.appendChild(item);
 }
